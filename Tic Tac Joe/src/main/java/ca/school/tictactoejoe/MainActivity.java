15f57831dfb0b7bca175e8432e6b11909d1262ce
Lance Thompson <sirlancealot77@gmail.com>
2014-04-22 22:48:49 -0700

    Game Running (Logic Errors)
    The game works with menu new game and exit game. There are errors with
    Joe's logic sometimes he lets the player win, when he shouldn't. Also
    once the game is over and there is still spots left you can click on one
    and you should not be able to.

--- Tic Tac Joe/src/main/java/ca/school/tictactoejoe/MainActivity.java
+++ Tic Tac Joe/src/main/java/ca/school/tictactoejoe/MainActivity.java
@@ -1,15 +1,17 @@
 package ca.school.tictactoejoe;
 
 import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.MenuInflater;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private TicTacJoeGame m_Game;
 
@@ -56,7 +58,6 @@ public class MainActivity extends ActionBarActivity {
         m_Game = new TicTacJoeGame();
         NewGame(); //actually start the game here
     }
-
     private void NewGame() //starting up a new game and clearing the board
     {
         m_Game.ClearBoard();
@@ -75,7 +76,7 @@ public class MainActivity extends ActionBarActivity {
         }
         else
         {
-            m_info.setText(R.string.computer_turn); //setting the text on the screen probably wont see thie because the computer moves so fast
+            m_info.setText(R.string.computer_turn); //setting the text on the screen probably wont see this because the computer moves so fast
             int computer_move = m_Game.getComputerMove(); //local variable for computers move
             setMove(m_Game.COMPUTER1, computer_move);
             m_playerFirst = true; //so when new games happens player goes first
@@ -85,45 +86,47 @@ public class MainActivity extends ActionBarActivity {
     {
         int location;
         public ButtonClickListener(int location)
-        {
+            {
             this.location = location;
         }
 
+        //check to see if the game is over pretty much
         @Override
-        public void onClick(View view) { //check to see if the game is over pretty much
-            if(m_gameOver && m_boardButton[location].isEnabled()){//run this if the game isnt ovev and the button is not disabled
-               setMove(m_Game.PLAYER1, location);
+        public void onClick(View view) {
+            if(!m_gameOver) {
+                //run this if the game isnt over and the button is not disabled
+                if (m_boardButton[location].isEnabled()) {
+                    setMove(m_Game.PLAYER1, location);
 
-                int winner = m_Game.WinnerCheck();
+                    int winner = m_Game.WinnerCheck();
 
-                if(winner == 0){
-                    m_info.setText(R.string.computer_turn); //this happens so fast we wont see this
-                    int move = m_Game.getComputerMove();
-                    setMove(m_Game.COMPUTER1, move); //setting the move of the computer
-                    winner = m_Game.WinnerCheck();
-                }
-                if(winner == 0) { //game is still going because the computer didnt win above
-                    m_info.setText(R.string.player_turn); //displaying its players turn
-                }
-                else if(winner == 1){ //the game was a tie
-                    m_info.setText(R.string.tie);//displaying text as tie
-                    m_tieCounter++;
-                    m_tieCount.setText(Integer.toString(m_tieCounter)); //updating the tie counter on the screen
-                    m_gameOver = true;
-                }
-                else if(winner == 2) { //the player has won at this point
-                    m_info.setText(R.string.player_win);//displaying text as tie
-                    m_playerCounter++;
-                    m_playerCount.setText(Integer.toString(m_playerCounter)); //updating the tie counter on the screen
-                    m_gameOver = true;
-                }
-                else{ //the computer has won and winner is 3
-                    m_info.setText(R.string.computer_win);//displaying text as tie
-                    m_computerCounter++;
-                    m_computerCount.setText(Integer.toString(m_computerCounter)); //updating the tie counter on the screen
-                    m_gameOver = true;
+                    if (winner == 0) {
+                        m_info.setText(R.string.computer_turn); //this happens so fast we wont see this
+                        int move = m_Game.getComputerMove();
+                        setMove(m_Game.COMPUTER1, move); //setting the move of the computer
+                        winner = m_Game.WinnerCheck();
+                    }
+                    if (winner == 0) { //game is still going because the computer didnt win above
+                        m_info.setText(R.string.player_turn); //displaying its players turn
+                    } else if (winner == 1) { //the game was a tie
+                        m_info.setText(R.string.tie);//displaying text as tie
+                        m_tieCounter++;
+                        m_tieCount.setText(Integer.toString(m_tieCounter)); //updating the tie counter on the screen
+                        m_gameOver = true;
+                    } else if (winner == 2) { //the player has won at this point
+                        m_info.setText(R.string.player_win);//displaying text as tie
+                        m_playerCounter++;
+                        m_playerCount.setText(Integer.toString(m_playerCounter)); //updating the tie counter on the screen
+                        m_gameOver = true;
+                    } else { //the computer has won and winner is 3
+                        m_info.setText(R.string.computer_win);//displaying text as tie
+                        m_computerCounter++;
+                        m_computerCount.setText(Integer.toString(m_computerCounter)); //updating the tie counter on the screen
+                        m_gameOver = true;
+                    }
                 }
             }
+            m_gameOver = false;
         }
     }
     private void setMove(char player, int location) //this set move calls tictacjoegame's setmove
@@ -151,6 +154,15 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId())
+        {
+            case R.id.new_game:
+                NewGame();
+                break;
+            case R.id.exit_game:
+                MainActivity.this.finish();
+                break;
+        }
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
